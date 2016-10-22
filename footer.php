a23ebb881245fbed0f3392604aea573af4071a6a
Dion Arya Pamungkas <zonamp3metal@gmail.com>
2016-10-22 21:05:35 +0700

    sdads

--- footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer -->
-<footer class="container-fluid bg-4 text-center">
-  <p>Dibuat Oleh <a href="http://fb.com/dion.aryapamungkas">&copy;Dion Arya Pamungkas</a></p>
-<p>Jangan Terlalu serius ini cuma bercanda</p>
-</footer>
-
-</body>
-</html>
